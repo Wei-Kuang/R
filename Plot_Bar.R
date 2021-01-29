cffe6f8c876a07715a5deef2b48051e37b6c32d8
WeiKuang <35211701+Wei-Kuang@users.noreply.github.com>
2021-01-28 20:45:53 -0800

    Add files via upload

--- Plot_Bar.R
+++ Plot_Bar.R
@@ -360,3 +360,33 @@ Missing_plot = function(data.arg, var_target, var_group, note='', my_legend_titl
     }
   return(merge_df)
 }
+
+
+
+#################
+#### Example ####
+#################
+
+
+#### Load iris data from ggplot2 package
+library(ggplot2)
+head(iris)
+DF = iris
+str(DF) # Note: Species is already a factor. 
+table(DF$Species) 
+
+
+#### Example-1 ####
+f_barplot_onevar(data.arg = DF,
+                 var_target = 'Species',
+                 y.limits.arg = c(0,100),
+                 y.breaks.arg = seq(0,100,20),
+                 coord_flip.arg = F)
+
+
+#### Example-2 ####
+f_barplot_onevar(data.arg = DF,
+                 var_target = 'Species',
+                 y.limits.arg = c(0,100),
+                 y.breaks.arg = seq(0,100,20),
+                 coord_flip.arg = T)
